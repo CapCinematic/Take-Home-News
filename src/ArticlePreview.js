e701c667765d54c88cd727b7454438e58e8cc633
Isaiah Richardson <capcinematic@gmail.com>
2023-09-14 11:29:14 -0600

    article css

--- src/ArticlePreview.js
+++ src/ArticlePreview.js
@@ -1,12 +1,13 @@
 import React from "react";
+import './ArticlePreview.css'
 
 function ArticlePreview({ article }) {
   return (
-    <div>
-      <img src={article.urlToImage} alt={article.title} />
-      <h3>{article.title}</h3>
-      <p>{article.description}</p>
-      <p>{article.publishedAt}</p>
+    <div className="article-preview">
+      <img className="preview-img" src={article.urlToImage} alt={article.title} />
+      <h3 className="preview-h3">{article.title}</h3>
+      <p className="preview-description">{article.description}</p>
+      <p className="preview-publishedAt">{article.publishedAt}</p>
     </div>
   );
 }
