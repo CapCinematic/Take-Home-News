f863037dfa98c4c99fcfa13c811af507cda15891
Isaiah Richardson <capcinematic@gmail.com>
2023-09-14 11:40:53 -0600

    basic data to show articles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ArticleList from './ArticleList';
+import ArticleDetail from './ArticleDetail';
 
 function App() {
 
--- src/ArticleDetail.js
+++ src/ArticleDetail.js
@@ -1,11 +1,14 @@
+import React from "react";
 
 function ArticleDetail({ article }) {
   return (
     <div>
-      <img src={article.urlToImage} alt={article.title} />
+      {/* <img src={article.url} alt={article.title} /> */}
       <h3>{article.title}</h3>
       <p>By:{article.author}</p>
       <p>{article.content}</p>
     </div>
   );
 }
+
+export default ArticleDetail
